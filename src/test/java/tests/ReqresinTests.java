7fd75eb4752c4930b420248322573945b096fddb
user <stel-vd@yandex.ru>
2022-06-07 00:20:43 +0300

    Added lessom tests

---
+++ src/test/java/tests/ReqresinTests.java
@@ -0,0 +1,45 @@
+package tests;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresinTests {
+    @Test
+    void loginTest() {
+        String body = "{ \"email\": \"eve.holt@reqres.in\", " +
+                "\"password\": \"cityslicka\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void missingPasswordLoginTest() {
+        String body = "{ \"email\": \"eve.holt@reqres.in\"}";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body(body)
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+}
